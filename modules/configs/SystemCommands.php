3f6bd1a2b9793546b6cb93f3fc6a57a5b18a0ec7
AwesomezGuy <jackpharley@gmail.com>
2011-02-11 12:11:22 +0000

    Added the ModuleConfig interface and an example module config

---
+++ modules/configs/SystemCommands.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * SystemCommands Module Config
+ *
+ * Copyright (c) 2011, Jack Harley
+ * All Rights Reserved
+ */
+ 
+namespace modules\configs;
+use awesomeircbot\modules\ModuleConfig;
+
+class SystemCommands implements ModuleConfig {
+	
+	public static $mappedCommands = array(
+		"quit" => "modules\QuitFromServer",
+	);
+	
+	public static $mappedEvents = array(
+	);
+	
+	public static $mappedTriggers = array(
+	);
+}
+?>
