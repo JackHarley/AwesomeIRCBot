d86bc5766a0e0f03215cf76f9bdc50f9d37b291c
Jack Harley <jackpharley@gmail.com>
2011-02-11 15:31:38 +0000

    Missed a few files in the last commit

---
+++ modules/PongServer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * PongServer Module
+ * Responds to a ping request from a server
+ *
+ * NOTE- THIS IS A SYSTEM MODULE, REMOVING IT MAY
+ * 	   REMOVE VITAL FUNCTIONALITY FROM THE BOT
+ *
+ * Copyright (c) 2011, Jack Harley
+ * All Rights Reserved
+ */
+namespace modules;
+
+use awesomeircbot\module\Module;
+use awesomeircbot\server\Server;
+
+class PongServer extends Module {
+	
+	public function run() {
+		$server = Server::getInstance();
+		$server->pong($this->senderNick);
+		echo "Sent pong\n";
+	}
+}
+?>
