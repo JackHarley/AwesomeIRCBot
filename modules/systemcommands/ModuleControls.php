d45331d0148885b9036f0d271f2df97c8c8461c0
Jack Harley <jackpharley@gmail.com>
2011-02-22 17:06:55 +0000

    Added full module dynamic load and unload support, along with a nice list view of modules

--- modules/systemcommands/ModuleControls.php
+++ modules/systemcommands/ModuleControls.php
@@ -27,12 +27,14 @@ class ModuleControls extends Module {
 		
 		switch ($action) {
 			case "load":
-				ModuleManager::loadModuleConfig($this->parameters(2));
+				$moduleNamespace = "modules\\" . strtolower($this->parameters(2)) . "\configs\\" . $this->parameters(2);
+				ModuleManager::loadModuleConfig($moduleNamespace);
 				$server->notify($this->senderNick, "Action completed");
 			break;
 			
 			case "unload":
-				ModuleManager::unloadModuleConfig($this->parameters(2));
+				$moduleNamespace = "modules\\" . strtolower($this->parameters(2)) . "\configs\\" . $this->parameters(2);
+				ModuleManager::unloadModuleConfig($moduleNamespace);
 				$server->notify($this->senderNick, "Action completed");
 			break;
 			
@@ -40,9 +42,17 @@ class ModuleControls extends Module {
 				$folder = opendir(__DIR__ . "/..");
 				$modulePacks = array();
 				while (($file = readdir($folder)) !== false) {
-					if (($file != ".") && ($file != "..") && ($file != "modules.inc.php"))
-						$modulePacks[] = $file;
+					if (($file != ".") && ($file != "..") && ($file != "modules.inc.php")) {
+						$folder2 = opendir(__DIR__ . "/../" . $file . "/configs");
+						while (($file2 = readdir($folder2)) !== false) {
+							if (($file2 != ".") && ($file2 != "..")) {
+								$modulePacks[] = str_replace(".php", "", $file2);
+							}
+						}
+						closedir($folder2);
+					}
 				}
+				closedir($folder);
 				
 				$server->message($this->senderNick, "************************************");
 				$server->message($this->senderNick, "Module Pack Listing");
@@ -55,7 +65,10 @@ class ModuleControls extends Module {
 					for($i=1;$i<$spacesToAdd;$i++) {
 						$spaces .= " ";
 					}
-					$server->message($this->senderNick, $modulePack . $spaces . " - " . chr(2) . chr(3) . "3Loaded" . chr(3) . chr(2));
+					if (ModuleManager::isLoaded($modulePack))
+						$server->message($this->senderNick, $modulePack . $spaces . " - " . chr(2) . chr(3) . "3Loaded" . chr(3) . chr(2));
+					else
+						$server->message($this->senderNick, $modulePack . $spaces . " - " . chr(2) . chr(3) . "4Not Loaded" . chr(3) . chr(2));
 					unset($spaces);
 				}
 				
