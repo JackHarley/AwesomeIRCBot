0115176e967cbb8a897f721c746b4653208e251e
Jack Harley <jackpharley@gmail.com>
2012-02-03 19:05:56 +0000

    Module data now gets stored in the database
    Module data now gets routinely mirrored back and forth from the database, this will allow for the web GUI to in future have an admin panel for modules.
    Module namespaces are now stored with asterisks instead of backslashes to get around SQL stripping backslashes

--- lib/awesomeircbot/data/DataManager.php
+++ lib/awesomeircbot/data/DataManager.php
@@ -28,8 +28,10 @@ class DataManager {
 		 if (!$module)
 		 	$module = $trace[1]['class'];
 		 
-		 if (DataManager::$data[$module][$request])
-		 	return DataManager::$data[$module][$request];
+		 $module = str_replace('\\', '*', $module);
+		 
+		 if (self::$data[$module][$request]["data"])
+		 	return self::$data[$module][$request]["data"];
 		 else
 		 	return false;
 	}
@@ -43,19 +45,71 @@ class DataManager {
 	 * @param mixed the data to store, supports objects, arrays, strings, etc.
 	 * @param string optionally, override the module name to store in another
 	 * module's space
+	 * @param int optionally, spoof the time this data was updated
 	 */
-	public function store($id, $data, $module=false) {
+	public function store($id, $data, $module=false, $lastUpdated=false) {
 		
 		$trace = debug_backtrace();
 		if (!$module)
 			$module = $trace[1]['class'];
 		
-		if (!DataManager::$data[$module])
-			DataManager::$data[$module] = array();
+		$module = str_replace('\\', '*', $module);
 		
-		DataManager::$data[$module][$id] = $data;
+		if (!$lastUpdated)
+			$lastUpdated = time();
+		
+		if (!self::$data[$module])
+			self::$data[$module] = array();
+		
+		self::$data[$module][$id] = array();
+		self::$data[$module][$id]["data"] = $data;
+		self::$data[$module][$id]["lastUpdated"] = $lastUpdated;
 		
 		return true;
+	}
+	
+	/**
+	 * Checks if there is stored data newer than a certain time
+	 *
+	 * @param string the identifier under which the data was stored
+	 * @param string the module name (e.g. modules\funstuff\Slap)
+	 * @param int the unix timestamp to check against
+	 * @return boolean true if data exists newer than the given timestamp,
+	 * otherwise, false
+	 */
+	public function checkIfDataExistsAndIsNewerThan($id, $module, $time) {
+		$module = str_replace('\\', '*', $module);
+		
+		if (self::$data[$module][$id]["lastUpdated"] > $time)
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * Gets the last updated time for the specified piece of data
+	 *
+	 * @param string the identifier under which the data was stored
+	 * @param string the module name (e.g. modules\funstuff\Slap)
+	 * @return int unix timestamp of last update to data or boolean false
+	 * if data does not exist
+	 */
+	public function getLastUpdatedTime($id, $module) {
+		$module = str_replace('\\', '*', $module);
+		
+		if (self::$data[$module][$request]["lastUpdated"])
+		 	return self::$data[$module][$request]["lastUpdated"];
+		 else
+		 	return false;
+	}
+	
+	/**
+	 * Gets all the data and returns it as an associative array
+	 *
+	 * @return array associative array of data
+	 */
+	public function getAllData() {
+		return self::$data;
 	}
 }
 ?>
