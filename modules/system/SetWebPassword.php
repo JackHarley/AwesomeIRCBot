d48a94323efe15abf6db537ec677cce49de6d3cc
AwesomezGuy <jackpharley@gmail.com>
2012-02-26 11:37:28 +0000

    Renamed the UpdateTopic module to FlushTopicCache in keeping with its meaning, and added 2 new modules for setting/generating user passwords (for the web interface)

---
+++ modules/system/SetWebPassword.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Set Web Password Module
+ * Allows fulfillment of a request
+ *
+ * NOTE- THIS IS A SYSTEM MODULE, REMOVING IT MAY
+ * 	   REMOVE VITAL FUNCTIONALITY FROM THE BOT
+ *
+ * Copyright (c) 2011, Jack Harley
+ * All Rights Reserved
+ */
+namespace modules\system;
+
+use awesomeircbot\module\Module;
+use awesomeircbot\config\Config;
+use awesomeircbot\server\Server;
+
+class SetWebPassword extends Module {
+	
+	public static $requireIdentification = true;
+	
+	public function run() {
+		
+		$server = Server::getInstance();
+		
+		$userPasswords = Config::getValue("userPasswords");
+		if (!$userPasswords)
+			$userPasswords = array();
+		
+		$userPasswords[$this->senderNick] = md5($this->parameters(1));
+		Config::setValue("userPasswords", $userPasswords);
+		
+		$server->notify($this->senderNick, "Password for " . $this->senderNick . " set to " . $this->parameters(1));
+	}
+}
+?>
