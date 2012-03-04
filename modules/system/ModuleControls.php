7ebc4e8d8cdedc010a952b4757387e16531f4cfb
AwesomezGuy <jackpharley@gmail.com>
2012-03-04 20:48:02 +0000

    Fixed a memory leak that could be caused by the existence of .DS_Store files in the /modules directory

--- modules/system/ModuleControls.php
+++ modules/system/ModuleControls.php
@@ -17,35 +17,35 @@ use awesomeircbot\module\ModuleManager;
 use awesomeircbot\server\Server;
 
 class ModuleControls extends Module {
-	
+
 	public static $requiredUserLevel = 10;
-	
+
 	public function run() {
-		
+
 		$action = $this->parameters(1);
 		$server = Server::getInstance();
-		
+
 		switch ($action) {
 			case "load":
 				$moduleNamespace = "modules\\" . strtolower($this->parameters(2)) . "\configs\\" . $this->parameters(2);
 				ModuleManager::loadModuleConfig($moduleNamespace);
 				$server->notify($this->senderNick, "Action completed");
 			break;
-			
+
 			case "unload":
 				$moduleNamespace = "modules\\" . strtolower($this->parameters(2)) . "\configs\\" . $this->parameters(2);
 				ModuleManager::unloadModuleConfig($moduleNamespace);
 				$server->notify($this->senderNick, "Action completed");
 			break;
-			
+
 			default:
 				$folder = opendir(__DIR__ . "/..");
 				$modulePacks = array();
 				while (($file = readdir($folder)) !== false) {
-					if (($file != ".") && ($file != "..") && ($file != "modules.inc.php")) {
+					if (($file != ".") && ($file != "..") && ($file != "modules.inc.php") && ($file != ".DS_Store")) {
 						$folder2 = opendir(__DIR__ . "/../" . $file . "/configs");
 						while (($file2 = readdir($folder2)) !== false) {
-							if (($file2 != ".") && ($file2 != "..")) {
+							if (($file2 != ".") && ($file2 != "..") && ($file2 != ".DS_Store")) {
 								$modulePacks[] = str_replace(".php", "", $file2);
 							}
 						}
@@ -53,15 +53,15 @@ class ModuleControls extends Module {
 					}
 				}
 				closedir($folder);
-				
+
 				$server->message($this->senderNick, "************************************");
 				$server->message($this->senderNick, "Module Pack Listing");
 				$server->message($this->senderNick, " ");
-				
+
 				foreach ($modulePacks as $modulePack) {
 					$modulePackLength = strlen($modulePack);
 					$spacesToAdd = 25 - $modulePackLength;
-					
+
 					for($i=1;$i<$spacesToAdd;$i++) {
 						$spaces .= " ";
 					}
@@ -71,10 +71,10 @@ class ModuleControls extends Module {
 						$server->message($this->senderNick, $modulePack . $spaces . " - " . chr(2) . chr(3) . "4Not Loaded" . chr(3) . chr(2));
 					unset($spaces);
 				}
-				
+
 				$server->message($this->senderNick, "************************************");
 			break;
-				
+
 		}
 	}
 }
